547138cffcf298a7bc2c33fbe340932d996130b3
Antonio Abraham <tony.calablava@gmail.com>
2019-08-21 02:58:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,5 +14,5 @@ end
 def find_the_cheese(i)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-   i.find(|i| i == "cheddar" || i == "gouda" || i == "camembert"}
+   i.find{|i| i == "cheddar" || i == "gouda" || i == "camembert"}
 end
