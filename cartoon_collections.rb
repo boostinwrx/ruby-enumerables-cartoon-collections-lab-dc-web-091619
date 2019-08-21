c9bfbd10c9789fb48afed5e77933a314b32eb9cc
Antonio Abraham <tony.calablava@gmail.com>
2019-08-21 02:17:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves(d)
   
-  d.each_with_index{|item,index| "#{index + 1}" + "#{item}"}
+  puts d.each_with_index{|item,index| "#{index + 1}" + "#{item}"}
 end
 
 def summon_captain_planet# code an argument here
