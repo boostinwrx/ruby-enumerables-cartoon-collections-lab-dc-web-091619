c9259cc3047a93c428dc0315372f689d951c98b5
Antonio Abraham <tony.calablava@gmail.com>
2019-08-21 02:34:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,8 @@ def long_planeteer_calls(l)
  l.any?{|w|w.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(i)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
 end
